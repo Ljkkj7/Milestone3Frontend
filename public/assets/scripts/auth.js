8fd9122c06f593465aaaca20fd68ee8aed6c215e
ljkkj7 <kianjenkins26@gmail.com>
2025-06-18 17:41:26 +0100

    Add - Login functionality using JWT

--- public/assets/scripts/auth.js
+++ public/assets/scripts/auth.js
@@ -23,3 +23,31 @@ document.getElementById('registerForm').onsubmit = async (e) => {
         alert("Request failed: " + err.message);
     }
 }
+
+document.getElementById('loginForm').onsubmit = async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    try {
+        const res = await fetch('https://marketio-3cedad1469b3.herokuapp.com/custom_auth/login/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await res.json();
+
+        if (res.ok) {
+            localStorage.setItem('access_token', data.access);
+            localStorage.setItem('refresh_token', data.refresh);
+            alert("Login successful");
+            window.location.href = 'market.html';
+        } else {
+            alert("Error: " + (data.detail || JSON.stringify(data)));
+        }
+    } catch(err) {
+        alert("Request failed: " + err.message);
+    }
+}
