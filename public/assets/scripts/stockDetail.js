899e121f618f5fb7dbb649aa3caa49cadf34a89b
ljkkj7 <kianjenkins26@gmail.com>
2025-06-21 13:47:33 +0100

    Changed script logic for stock detail

--- public/assets/scripts/stockDetail.js
+++ public/assets/scripts/stockDetail.js
@@ -8,27 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const params = new URLSearchParams(window.location.search);
     const symbol = params.get('symbol');
-    const container = document.getElementById('stock-detail');
+
+    document.getElementById('stockTitle').textContent = symbol;
 
     // let priceHistory = JSON.parse(localStorage.getItem('priceHistory'));
     // priceHistory[symbol] = priceHistory[symbol] || [];
-    // let previousPrice = priceHistory[symbol][priceHistory[symbol].length - 1] || 0;
 
-    const stockCanvas = document.createElement('div');
-    stockCanvas.className = 'stock-card';
-    stockCanvas.innerHTML = `
-    <div class="stock-title">${symbol}</div>
-    <a href="stock-detail.html?symbol=${symbol}" class="stock-link">
-        <div class="stock-price" id="price-${symbol}">£${parseFloat(price).toFixed(2)}</div>
-        <canvas id="chart-${symbol}" width="400" height="200"></canvas>
-    </a>`;
-    console.log(`Creating new individual chart for symbol: ${symbol}`);
-    container.appendChild(stockCanvas);
-    renderedStocks.add(symbol); // Add to rendered stocks set
-    
-    const stockCtx = document.getElementById(`chart-${symbol}`).getContext('2d');
-    // Create a new stock chart for this symbol
-    createStockChart(stockCtx, symbol);
+    const chartCtx = document.getElementById('stockChart').getContext('2d');
+    // let previousPrice = priceHistory[symbol][priceHistory[symbol].length - 1] || 0;
+    createStockChart(chartCtx, symbol);
 
     socket.on('stocks_data', (stocks) => {
         console.log("Recieved paramater", stocks)
