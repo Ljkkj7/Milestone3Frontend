df0cdf16bcb50b94fb2ef0451c9526ea92e6d9f6
ljkkj7 <kianjenkins26@gmail.com>
2025-07-02 16:14:32 +0100

    Debugging

--- public/assets/scripts/executeTrade.js
+++ public/assets/scripts/executeTrade.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             valueHeld.innerHTML = `
                 <h3>Total Value Held:</h3>
-                <p>£${buyData.balance}</p>
+                <p>£${buyData.quantity*buyData.price}</p>
             `
 
         } catch(err) {
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 valueHeld.innerHTML = `
                         <h3>Total Value Held:</h3>
-                        <p>£${sellData.balance}</p>
+                        <p>£${sellData.quantity*sellData.price}</p>
                     `
             }
         } catch (err) {
