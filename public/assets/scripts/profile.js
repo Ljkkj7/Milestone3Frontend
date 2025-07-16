2ed585912278cae1080e10aaf83ce997b121c4e2
ljkkj7 <kianjenkins26@gmail.com>
2025-07-16 15:33:21 +0100

    Debugging

--- public/assets/scripts/profile.js
+++ public/assets/scripts/profile.js
@@ -55,7 +55,7 @@ function appendComment(comment) {
         const userData = parseJwt(token);
         // You can use userData to customize the comment display
 
-        if (userData.user_id === comment.author_id || userData.user_id === getUserIdFromUrl()) {
+        if (userData.user_id === comment.author_id || userData.user_id === Number(getUserIdFromUrl())) {
             console.log(userData.user_id, comment.author_id, getUserIdFromUrl());
             div.innerHTML += `
                 <button class="CRUD-comment" id="del${comment.id}">Delete</button>
