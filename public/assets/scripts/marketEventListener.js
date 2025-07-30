a4c21353e335d89323889a63af18107f31b9c7d2
ljkkj7 <kianjenkins26@gmail.com>
2025-07-30 15:09:30 +0100

    Set up of event listener for market events to display events to users

---
+++ public/assets/scripts/marketEventListener.js
@@ -0,0 +1,22 @@
+import { getEventTemplate } from "./eventTemplates.js";
+
+const socket = io.connect();
+
+socket.on('market_event', (marketEvent) => {
+    showEventBanner(marketEvent.symbol, marketEvent.type);
+})
+
+function showEventBanner(symbol, type){
+    const eventContainer = document.getElementById('market-event-container');
+    const eventDiv = document.createElement('div');
+    const html = getEventTemplate(type, symbol);
+
+    eventDiv.className = 'market-event';
+    eventDiv.innerHTML = html;
+
+    eventContainer.append(eventDiv);
+
+    setTimeout(() => {
+        eventDiv.remove();
+    }, 10000);
+}
