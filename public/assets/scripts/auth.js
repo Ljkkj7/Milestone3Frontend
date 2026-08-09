ec197f7eff22b89aea3039939ffbaa7b24766345
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-08-09 13:51:44 +0100

    Update auth.js

--- public/assets/scripts/auth.js
+++ public/assets/scripts/auth.js
@@ -1,57 +1,62 @@
 
-
-document.getElementById('registerForm').onsubmit = async (e) => {
-    e.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const email = document.getElementById('email').value;
-
-    try {
-        const res = await fetch('https://marketio-3cedad1469b3.herokuapp.com/custom_auth/register/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ username, email, password })
-        });
-
-        const data = await res.json();
-
-        if (res.ok) {
-            alert("User registered");
-        } else {
-            alert("Error: " + (data.detail || JSON.stringify(data)));
+const regsiterForm = document.getElementById('registerForm')
+if (registerForm){
+    registerForm.onsubmit = async (e) => {
+        e.preventDefault();
+    
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const email = document.getElementById('email').value;
+    
+        try {
+            const res = await fetch('https://marketio-3cedad1469b3.herokuapp.com/custom_auth/register/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ username, email, password })
+            });
+    
+            const data = await res.json();
+    
+            if (res.ok) {
+                alert("User registered");
+            } else {
+                alert("Error: " + (data.detail || JSON.stringify(data)));
+            }
+        } catch(err) {
+            alert("Request failed: " + err.message);
         }
-    } catch(err) {
-        alert("Request failed: " + err.message);
     }
 }
 
-document.getElementById('loginForm').onsubmit = async (e) => {
-    e.preventDefault();
-
-    const navLinksToggle = document.getElementsByClassName('loggedIn');
-    const username = document.getElementById('loginUsername').value;
-    const password = document.getElementById('loginPassword').value;
-
-    try {
-        const res = await fetch('https://marketio-3cedad1469b3.herokuapp.com/custom_auth/login/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ username, password })
-        });
-
-        const data = await res.json();
-
-        if (res.ok) {
-            localStorage.setItem('access_token', data.access);
-            localStorage.setItem('refresh_token', data.refresh);
-            alert("Login successful");
-            window.location.href = 'market.html';
-        } else {
-            alert("Error: " + (data.detail || JSON.stringify(data)));
+const loginForm = document.getElementById('loginForm')
+if (loginForm) {
+    loginForm.onsubmit = async (e) => {
+        e.preventDefault();
+    
+        const navLinksToggle = document.getElementsByClassName('loggedIn');
+        const username = document.getElementById('loginUsername').value;
+        const password = document.getElementById('loginPassword').value;
+    
+        try {
+            const res = await fetch('https://marketio-3cedad1469b3.herokuapp.com/custom_auth/login/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ username, password })
+            });
+    
+            const data = await res.json();
+    
+            if (res.ok) {
+                localStorage.setItem('access_token', data.access);
+                localStorage.setItem('refresh_token', data.refresh);
+                alert("Login successful");
+                window.location.href = 'market.html';
+            } else {
+                alert("Error: " + (data.detail || JSON.stringify(data)));
+            }
+        } catch(err) {
+            alert("Request failed: " + err.message);
         }
-    } catch(err) {
-        alert("Request failed: " + err.message);
     }
 }
 
